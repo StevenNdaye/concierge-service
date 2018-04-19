ad644a2d6f22916302cad6c913a09cf230cb7f63
StevenNdaye <stevenndaye@gmail.com>
2018-04-19 21:10:54 +0200

    Initial set up

---
+++ src/main/java/com/concierge/entity/City.java
@@ -0,0 +1,133 @@
+package com.concierge.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String country;
+    @Column(name = "fun_fact")
+    private String fun_fact;
+    @Column(length = 500)
+    private String description;
+    private double latitude;
+    private double longitude;
+
+    public City(int id, String name, String country, String fun_fact, String description, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.fun_fact = fun_fact;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public City() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFun_fact() {
+        return fun_fact;
+    }
+
+    public void setFun_fact(String fun_fact) {
+        this.fun_fact = fun_fact;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        City city = (City) o;
+
+        if (id != city.id) return false;
+        if (Double.compare(city.latitude, latitude) != 0) return false;
+        if (Double.compare(city.longitude, longitude) != 0) return false;
+        if (name != null ? !name.equals(city.name) : city.name != null) return false;
+        if (country != null ? !country.equals(city.country) : city.country != null) return false;
+        if (fun_fact != null ? !fun_fact.equals(city.fun_fact) : city.fun_fact != null) return false;
+        return description != null ? description.equals(city.description) : city.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (fun_fact != null ? fun_fact.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        temp = Double.doubleToLongBits(latitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", fun_fact='" + fun_fact + '\'' +
+                ", description='" + description + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
