3a15f5c1761be292febe60fa5452402b11b01558
StevenNdaye <stevenndaye@gmail.com>
2018-04-19 21:29:06 +0200

    Seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO cities
+INSERT INTO cities (id, name, country, fun_fact, description, latitude, longitude)
 VALUES (1, 'Johannesburg', 'South Africa', 'Awesome city of gold but the bus drivers are particularly skilled',
         'Johannesburg, South Africa''s biggest city and capital of Gauteng province, began as a 19th-century gold-mining settlement.
         Its sprawling Soweto township was once home to Nelson Mandela and Desmond Tutu. Mandela’s former residence is now the
         Mandela House museum. Other Soweto museums that recount the struggle to end segregation include the somber Apartheid Museum
-        and Constitution Hill, a former prison complex',26.2041, 28.0473);
+        and Constitution Hill, a former prison complex', 26.2041, 28.0473);
 
-INSERT INTO cities
+INSERT INTO cities (id, name, country, fun_fact, description, latitude, longitude)
 VALUES (2, 'Kinshasa', 'Democratic Republic of Congo', 'Kinshasa la belle, Kinshasa elengi',
         'Kinshasa is the capital and the largest city of the Democratic Republic of the Congo. The city is situated alongside the Congo River.'
-        ,4.4419, 15.2663);
+  , 4.4419, 15.2663);
