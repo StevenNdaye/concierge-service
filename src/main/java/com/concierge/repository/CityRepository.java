505c500e478834e2d03a5c01d8572dfaaee84bcf
StevenNdaye <stevenndaye@gmail.com>
2018-04-20 00:20:25 +0200

    Delete not needed files

--- src/main/java/com/concierge/repository/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.concierge.repository;
-
-import com.concierge.entity.City;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CityRepository extends CrudRepository<City, Integer> {
-
-}
